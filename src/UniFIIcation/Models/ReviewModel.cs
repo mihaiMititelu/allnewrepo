2ec0bdc1c2dece51996cc06dc3dc146fd585e9fc
Rancu Marius <marius.rancu@hotmail.com>
2017-01-17 09:23:14 +0200

    Integrat modulul Review

---
+++ src/UniFIIcation/Models/ReviewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReviewWebSite.Models
+{
+    public class ReviewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        private DateTime _insDate = DateTime.Now;
+
+        public DateTime DateTime
+        {
+            get { return _insDate; }
+            set { _insDate = value; }
+        }
+
+        //public DateTime DateTime { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Comment { get; set; }
+
+        [Required]
+        public string Rating { get; set; }
+    }
+
+}
+
