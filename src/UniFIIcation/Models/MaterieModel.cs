6c518264100187286404d1c35f3eda93a509fa24
VITALAIDER <VITALAIDER@GMAIL.COM>
2017-01-17 13:45:52 +0200

    Add Models for Materii
    am sters modelul vechi

---
+++ src/UniFIIcation/Models/MaterieModel.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Collections.Generic;
+
+namespace ProiectDeTest8.Models
+{
+    public class Materie
+    {
+
+        public int MaterieId { get; set; }
+        public string Name { get; set; }
+
+        public List<Postare> Postari { get; set; }
+        public List<Upload> Uploads { get; set; }
+    }
+}
---
+++ src/UniFIIcation/Models/UploadModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProiectDeTest8.Models
+{
+    public class Upload
+    {
+        [Required]
+        public Guid UploadId { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+
+        private DateTime _insDate = DateTime.Now;
+        public DateTime DateTimeUpload
+        {
+            get { return _insDate; }
+            set { _insDate = value; }
+        }
+
+        public Guid UserId { get; set; }
+        public int MaterieId { get; set; }
+        public Materie Materie { get; set; }
+    }
+}
